8a0a47be98714745bfe1f32e13b44475cb30c60f
oWave <oWave@users.noreply.github.com>
2023-06-20 22:38:21 +0200

    Show DC to all

--- src/flat.ts
+++ src/flat.ts
@@ -1,9 +1,12 @@
-export async function rollFlatCheck(dc: number, { hidden = false, label }: { hidden: boolean, label?: string }) {
+export async function rollFlatCheck(
+  dc: number,
+  { hidden = false, label }: { hidden: boolean; label?: string }
+) {
   const r = await new Roll("d20").roll()
   const degree = r.total >= dc ? 2 : 1
 
   // @ts-expect-error pf2e
-  const flavor = await game.pf2e.Check.createResultFlavor({
+  const flavor: HTMLElement = await game.pf2e.Check.createResultFlavor({
     degree: {
       value: degree,
       unadjusted: degree,
@@ -14,12 +17,13 @@ export async function rollFlatCheck(dc: number, { hidden = false, label }: { hid
     },
   })
 
-  r.toMessage({ flavor: flavor.outerHTML }, { rollMode: hidden ? "blindroll" : "roll" })
-
-  // r.toMessage({}, { create: true })
+  r.toMessage(
+    { flavor: flavor.outerHTML.replaceAll('data-visibility="gm"', "") },
+    { rollMode: hidden ? "blindroll" : "roll" }
+  )
 }
 
-const DCS = {
+export const CONDITION_DCS = {
   concealed: 5,
   hidden: 11,
   invisible: 11,
@@ -29,17 +33,20 @@ function dcForToken(token: Token) {
   let dc = 0
   // @ts-expect-error pf2e
   token.actor?.conditions.forEach((c) => {
-    dc = Math.max(DCS[c.slug] ?? 0, dc)
+    dc = Math.max(CONDITION_DCS[c.slug] ?? 0, dc)
   })
   return dc || null
 }
 
-export async function rollForSingleTarget(target: Token | undefined, { hidden = false }: { hidden: boolean }) {
+export async function rollForSingleTarget(
+  target: Token | undefined,
+  { hidden = false }: { hidden: boolean }
+) {
   if (!target) return
   const dc = dcForToken(target)
   if (!dc)
     ui.notifications.warn(
       "Selected target has no conditions that require a flat check"
     )
-  else rollFlatCheck(dc, {hidden})
+  else rollFlatCheck(dc, { hidden })
 }
