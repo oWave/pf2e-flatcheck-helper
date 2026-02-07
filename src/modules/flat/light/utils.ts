182da2e1a21afb341185ed74e195bb5905b587ea
oWave <oWave@users.noreply.github.com>
2026-02-07 20:06:09 +0100

    fix type

--- src/modules/flat/light/utils.ts
+++ src/modules/flat/light/utils.ts
@@ -64,7 +64,7 @@ export const TargetColors = Object.freeze({
 	},
 })
 
-export type LightLevel = typeof LightLevels.DARK
+export type LightLevel = (typeof LightLevels)["DARK" | "BRIGHT" | "DIM"]
 
 export function darknessAtPoint(x: number, y: number): number {
 	for (const source of (canvas.effects as any).darknessSources as Collection<
